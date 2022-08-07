98c5f04cd82546b605802e6033de3698ddcb48a6
Sritiman Adak <adaksritiman24@gmail.com>
2022-08-07 13:46:19 +0530

    added jenkins job DSL

---
+++ jenkins.groovy
@@ -0,0 +1,19 @@
+job('node js example'){
+    scm{
+        git('git://github.com/adaksritiman24/first-jenkins-demo.git'){ node ->
+            node / gitConfigName('adaksritiman24')
+            node / gitConfigEmail('adaksritiman24@gmail.com')
+        }
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+
+    steps{
+        shell('npm install')
+    }
+}
